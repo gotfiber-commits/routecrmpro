960add66eeded6f35a1163135e868962fbb75e3a
gotfiber-commits <gotfiber@gmail.com>
2026-02-01 17:34:04 -0600

    Update setup.js

--- netlify/functions/setup.js
+++ netlify/functions/setup.js
@@ -1,23 +1,13 @@
 // One-time setup endpoint - DELETE THIS FILE after use!
 const { query } = require('./utils/db');
 const bcrypt = require('bcryptjs');
-const { success, error, handleOptions } = require('./utils/response');
+const { success, error } = require('./utils/response');
 
 exports.handler = async (event, context) => {
-    if (event.httpMethod === 'OPTIONS') {
-        return handleOptions();
-    }
-
-    if (event.httpMethod !== 'POST') {
-        return error('POST required', 405);
-    }
-
     try {
-        // Generate proper bcrypt hash
         const password = 'superadmin123';
         const hash = await bcrypt.hash(password, 10);
         
-        // Delete existing and insert new
         await query('DELETE FROM super_admins WHERE username = $1', ['superadmin']);
         
         await query(
@@ -30,10 +20,9 @@ exports.handler = async (event, context) => {
             message: 'Super admin created!',
             username: 'superadmin',
             password: 'superadmin123',
-            warning: 'DELETE the setup.js file from your repo after this!'
+            warning: 'DELETE the setup.js file from your repo now!'
         });
     } catch (err) {
-        console.error('Setup error:', err);
         return error('Setup failed: ' + err.message, 500);
     }
 };
